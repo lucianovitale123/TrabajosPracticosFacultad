92147fd9162d3299ed8b07be00be6eca712a5ba4
DESKTOP-NITA170\Pili <pilarmosto@gmail.com>
2023-08-24 18:59:29 -0300

    ArrayList

--- src/Ejercicio3/mainEjercicio3.java
+++ src/Ejercicio3/mainEjercicio3.java
@@ -1,5 +1,7 @@
 package Ejercicio3;
 
+import java.util.ArrayList;
+
 public class mainEjercicio3 {
 
 	public static void main(String[] args) {
@@ -11,5 +13,12 @@ public class mainEjercicio3 {
 		Oficina o = new Oficina(1, 500, 1);
 		System.out.println(o.toString());
 		
+		ArrayList<IEdificio> Listaedificios = new ArrayList<>(5);
+		
+		Listaedificios.add(new Polideportivo(3000, 1, "Polideportivo 1"));
+		Listaedificios.add(new Polideportivo(1500, 2, "Polideportivo 2"));
+		Listaedificios.add(new Polideportivo(4000, 3, "Polideportivo 3"));
+		Listaedificios.add(new Oficina(30, 800, 1));
+		Listaedificios.add(new Oficina(20, 400, 2));
 	}
 }
