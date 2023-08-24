30d78a446746da7bb9be5d88eb9d8f840e33498f
Guido Benetti <guidox.alucard@gmail.com>
2023-08-23 22:44:30 -0300

    Inicie el ejercicio 2 creando Producto y producto fresco

---
+++ src/Ejercicio2/Producto.java
@@ -0,0 +1,33 @@
+package Ejercicio2;
+
+public class Producto {
+	private int NroLote;
+	private String FechaDeCaducidad;
+	
+	
+	//Constructores
+	public Producto() {
+		this.NroLote=999;
+		this.FechaDeCaducidad="01/01/2001";
+	}
+	public Producto(String Fecha, int lote) {
+		this.NroLote=lote;
+		this.FechaDeCaducidad=Fecha;
+	}
+	
+	public int getNroLote() {
+		return NroLote;
+	}
+	public void setNroLote(int nroLote) {
+		NroLote = nroLote;
+	}
+	public String getFechaDeCaducidad() {
+		return FechaDeCaducidad;
+	}
+	public void setFechaDeCaducidad(String fechaDeCaducidad) {
+		FechaDeCaducidad = fechaDeCaducidad;
+	}
+	
+	
+
+}
---
+++ src/Ejercicio2/ProductoFresco.java
@@ -0,0 +1,32 @@
+package Ejercicio2;
+
+public class ProductoFresco extends Producto {
+	private String FechaDeEnvasado;
+	private String PaisDeOrigen;
+	
+	//Constructores
+	public ProductoFresco() {
+		this.PaisDeOrigen="Argentina";
+		this.FechaDeEnvasado="01/01/2001";
+	}
+	public ProductoFresco(String Fecha, String pais) {
+		this.PaisDeOrigen=pais;
+		this.FechaDeEnvasado=Fecha;
+	}
+	
+	public String getFechaDeEnvasado() {
+		return FechaDeEnvasado;
+	}
+	public void setFechaDeEnvasado(String fechaDeEnvasado) {
+		FechaDeEnvasado = fechaDeEnvasado;
+	}
+	public String getPaisDeOrigen() {
+		return PaisDeOrigen;
+	}
+	public void setPaisDeOrigen(String paisDeOrigen) {
+		PaisDeOrigen = paisDeOrigen;
+	}
+	
+	
+
+}
