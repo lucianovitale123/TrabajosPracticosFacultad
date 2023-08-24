17872be371aca7ed1cb3501982a6c45ed95a4add
lvitale <lucianovitale00@gmail.com>
2023-08-24 20:32:59 -0300

    finalizando el punto 3 y 2

---
+++ src/Ejercicio2/mainEjercicio2.java
@@ -0,0 +1,26 @@
+package Ejercicio2;
+
+
+
+public class mainEjercicio2 {
+	
+	public static void main(String[] args) {
+		
+		//Casteos de las clases 
+		
+		Producto producto = new Producto();
+		ProductoCongelado pCongelado = new ProductoCongelado();
+		ProductoFresco pFresco = new ProductoFresco();
+		ProductoRefrigerado pRefrigerado = new ProductoRefrigerado();
+		
+		System.out.println(producto.toString());
+		System.out.println(pCongelado.toString());
+		System.out.println(pFresco.toString());
+		System.out.println(pRefrigerado.toString());
+		
+
+	}
+	
+	
+	
+}
--- src/Ejercicio3/mainEjercicio3.java
+++ src/Ejercicio3/mainEjercicio3.java
@@ -1,6 +1,7 @@
 package Ejercicio3;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class mainEjercicio3 {
 
@@ -13,12 +14,22 @@ public class mainEjercicio3 {
 		Oficina o = new Oficina(1, 500, 1);
 		System.out.println(o.toString());
 		
-		ArrayList<IEdificio> Listaedificios = new ArrayList<>(5);
+		ArrayList<IEdificio> listaEdificios = new ArrayList<>(5);
 		
-		Listaedificios.add(new Polideportivo(3000, 1, "Polideportivo 1"));
-		Listaedificios.add(new Polideportivo(1500, 2, "Polideportivo 2"));
-		Listaedificios.add(new Polideportivo(4000, 3, "Polideportivo 3"));
-		Listaedificios.add(new Oficina(30, 800, 1));
-		Listaedificios.add(new Oficina(20, 400, 2));
+		listaEdificios.add(new Polideportivo(3000, 1, "Polideportivo 1"));
+		listaEdificios.add(new Polideportivo(1500, 2, "Polideportivo 2"));
+		listaEdificios.add(new Polideportivo(4000, 3, "Polideportivo 3"));
+		listaEdificios.add(new Oficina(30, 800, 1));
+		listaEdificios.add(new Oficina(20, 400, 2));
+		
+		System.out.println("Lista de edificios");
+		
+		ListIterator<IEdificio> it = listaEdificios.listIterator();
+		while(it.hasNext()) {
+			IEdificio edificio = it.next();
+			it.remove();
+			System.out.println(edificio.toString());
+		}
+		
 	}
 }
