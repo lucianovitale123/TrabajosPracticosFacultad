44f5af8661cb097f8a9723c14a7b3b60a28d2f90
Guido Benetti <guidox.alucard@gmail.com>
2023-08-24 15:30:06 -0300

    Agregue main, hice metodos en polideportivo, gets y sets y tostring

--- src/Ejercicio3/Polideportivo.java
+++ src/Ejercicio3/Polideportivo.java
@@ -3,32 +3,61 @@ package Ejercicio3;
 public class Polideportivo implements IinstalacionDeportiva, IEdificio {
 	
 	//Propiedades
+	private double superficieEdificio;
+	private int tipoDeInstalacion;
+	private String nombrePolideportivo;
 	
+	//Constructors
+	public Polideportivo() {
+		super();
+		this.superficieEdificio = 500;
+		this.tipoDeInstalacion = 1 ;
+		this.nombrePolideportivo = "Default";
+	}
 	
-	
+	public Polideportivo(double superficieEdi, int tipoInstalacion, String nombrePoli) {
+		super();
+		this.superficieEdificio =  superficieEdi;
+		this.tipoDeInstalacion =  tipoInstalacion;
+		this.nombrePolideportivo = nombrePoli;
+	}
 	
 	//Sets y gets
 	
+	public void setSuperficieEdificio(double superficieEdificio) {
+		this.superficieEdificio = superficieEdificio;
+	}
+
+	public void setTipoDeInstalacion(int tipoDeInstalacion) {
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
 	
-	
-	
-	
-	
-	
+	public void setNombrePolideportivo(String nombrePolideportivo) {
+		this.nombrePolideportivo = nombrePolideportivo;
+	}
+
+	public String getNombrePolideportivo() {
+		return nombrePolideportivo;
+	}	
 	// Metodos
 	
-
 	@Override
 	public double getSuperficieEdificio() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.superficieEdificio;
 	}
 
 	@Override
 	public int getTipoDeInstalacion() {
-		// TODO Auto-generated method stub
-		return 0;
+		return tipoDeInstalacion;
 	}
+
+	@Override
+	public String toString() {
+		return "Polideportivo:" + nombrePolideportivo;
+	}
+	
+	
 	
 	
 
---
+++ src/Ejercicio3/mainEjercicio3.java
@@ -0,0 +1,14 @@
+package Ejercicio3;
+
+public class mainEjercicio3 {
+
+	public static void main(String[] args) {
+		
+		Polideportivo p= new Polideportivo();
+		p.setNombrePolideportivo("El gran escobar");
+		
+		System.out.println(p.toString());
+
+	}
+
+}
