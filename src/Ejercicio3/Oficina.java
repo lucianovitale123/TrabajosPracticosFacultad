47f7ccfce8f80a01d53a6954d105cc19c1829134
LAPTOP-5OMBHCP7\zoero <zoerouco@gmail.com>
2023-08-24 21:07:59 -0300

    un edificio no es una instalacion deportiva!!!! siempre se aprende algo nuevo xd

--- src/Ejercicio3/Oficina.java
+++ src/Ejercicio3/Oficina.java
@@ -1,22 +1,19 @@
 package Ejercicio3;
 
-public class Oficina implements IEdificio, IinstalacionDeportiva{
+public class Oficina implements IEdificio{
 	//Atributos
 	private int nroOficinas;
 	private double superficieEdificio;
-	private int tipoDeInstalacion;
 
 	//Constructores
 	public Oficina() {
 		nroOficinas = -1;
 		superficieEdificio = -1;
-		tipoDeInstalacion = -1;
 	}
 	
-	public Oficina(int nroOficinas, double superficieEdificio, int tipoDeInstalacion) {
+	public Oficina(int nroOficinas, double superficieEdificio) {
 		this.nroOficinas = nroOficinas;
 		this.superficieEdificio = superficieEdificio;
-		this.tipoDeInstalacion = tipoDeInstalacion;
 	}
 	
 	//Sets y Gets
@@ -31,17 +28,8 @@ public class Oficina implements IEdificio, IinstalacionDeportiva{
 	public void setSuperficieEdificio(double superficieEdificio) {
 		this.superficieEdificio = superficieEdificio;
 	}
-
-	public void setTipoDeInstalacion(int tipoDeInstalacion) {
-		this.tipoDeInstalacion = tipoDeInstalacion;
-	}
 	
 	//Metodos de interfaz (gets)
-	@Override
-	public int getTipoDeInstalacion() {
-		return this.tipoDeInstalacion;
-	}
-
 	@Override
 	public double getSuperficieEdificio() {
 		return this.superficieEdificio;
@@ -50,8 +38,7 @@ public class Oficina implements IEdificio, IinstalacionDeportiva{
 	//Metodos
 	@Override
 	public String toString() {
-		return "Oficina numero:" + nroOficinas + ", Superficie del edificio:" + superficieEdificio
-				+ ", Tipo de instalacion:" + tipoDeInstalacion;
+		return "Oficina numero:" + nroOficinas + ", Superficie del edificio:" + superficieEdificio;
 	}
 
 	@Override
@@ -62,7 +49,6 @@ public class Oficina implements IEdificio, IinstalacionDeportiva{
 		long temp;
 		temp = Double.doubleToLongBits(superficieEdificio);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + tipoDeInstalacion;
 		return result;
 	}
 
@@ -79,8 +65,7 @@ public class Oficina implements IEdificio, IinstalacionDeportiva{
 			return false;
 		if (Double.doubleToLongBits(superficieEdificio) != Double.doubleToLongBits(other.superficieEdificio))
 			return false;
-		if (tipoDeInstalacion != other.tipoDeInstalacion)
-			return false;
 		return true;
 	}
+	
 }
--- src/Ejercicio3/mainEjercicio3.java
+++ src/Ejercicio3/mainEjercicio3.java
@@ -6,21 +6,14 @@ import java.util.ListIterator;
 public class mainEjercicio3 {
 
 	public static void main(String[] args) {
-		
-		Polideportivo p= new Polideportivo();
-		p.setNombrePolideportivo("El gran escobar");
-		System.out.println(p.toString());
-		
-		Oficina o = new Oficina(1, 500, 1);
-		System.out.println(o.toString());
-		
+
 		ArrayList<IEdificio> listaEdificios = new ArrayList<>(5);
 		
 		listaEdificios.add(new Polideportivo(3000, 1, "Polideportivo 1"));
 		listaEdificios.add(new Polideportivo(1500, 2, "Polideportivo 2"));
 		listaEdificios.add(new Polideportivo(4000, 3, "Polideportivo 3"));
-		listaEdificios.add(new Oficina(30, 800, 1));
-		listaEdificios.add(new Oficina(20, 400, 2));
+		listaEdificios.add(new Oficina(30, 800));
+		listaEdificios.add(new Oficina(20, 400));
 		
 		System.out.println("Lista de edificios");
 		
